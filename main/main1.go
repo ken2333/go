acb81e5df7f56d416b10b1b5c68c8de42f58649d
ken2333 <1176497634@qq.com>
2019-04-25 01:24:24 +0800

    第一天

--- main/main1.go
+++ main/main1.go
@@ -3,5 +3,6 @@ package main //main包是一个主函数，作为一个入口的标记
 import "fmt" //引入包
 
 func main() {
-	fmt.Printf("hello world golang")
+	fmt.Println("hello world golang")
+	fmt.Println("hello world golang")
 }
